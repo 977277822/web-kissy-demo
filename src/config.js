ddd31ee96058e50130e710ebb8dab1a2f2a82e31
zhangxiaobin <977277822@qq.com>
2015-06-03 11:50:24 +0800

    提交

---
+++ src/config.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 2015/6/3.
+ */
+
+(function(){
+    require.config({
+        paths : {
+            "dom" : "../src/jquery/jquip",
+            "css" : "../src/jquery/jquip.css",
+            "ajax" : "../src/jquery/jquip.ajax",
+            "event" : "../src/jquery/jquip.events"
+        }
+    });
+})();
